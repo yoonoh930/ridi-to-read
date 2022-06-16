6298dcf885ef5103b4b6f5ab3ce481e17268d3ae
Yoon Oh <yoonoh930@gmail.com>
2022-06-16 16:17:20 -0700

    implements the essentials

--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,38 @@
 # Ridi-to-Read
 
 This project is to help convert the ridibooks highlights drag-and-drop format to csv format for the readwise csv import format.
+
+## How to use
+
+```shell
+python3 name_of_the_file -t "Book Title" -a "Author Name"
+```
+
+## Format of the text file
+
+It should be copied such that 
+
+```
+Highlights
+
+[Note]
+
+Date stamp
+```
+
+like
+
+```
+죽음은 모든 것을 끝내며 따라서 포괄적인 결론이다
+
+2021.01.31.
+
+그의 삶이 끝난 후 그가 지구상에 체류했음을 말해 주는 흔적은 강물에 던져진 돌이 수면에 남기는 흔적만큼도 못 될지도 모른다
+
+2021.01.31.
+
+그리고 그 모든 것 뒤에는 모험과 영웅 같은 것에 대한 열렬한 동경이 자리하고 있어
+
+2021.01.31.
+```
+
--- main.py
+++ main.py
@@ -1,16 +1,89 @@
-# This is a sample Python script.
+import argparse
+from dateutil.parser import parse
+import pandas as pd
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def is_date(string, fuzzy=False):
+    """
+    Return whether the string can be interpreted as a date.
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    :param string: str, string to check for date
+    :param fuzzy: bool, ignore unknown tokens in string if True
+
+    Source: https://stackoverflow.com/questions/25341945/check-if-string-has-date-any-format
+    """
+    try:
+        parse(string, fuzzy=fuzzy)
+        return True
+
+    except ValueError:
+        return False
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "file_name", help="File name of highlights to convert with its suffix"
+    )
+    parser.add_argument(
+        "-t", "--title", nargs="?", const="arg_was_not_given", help="Name of the book"
+    )
+    parser.add_argument(
+        "-a",
+        "--author",
+        nargs="?",
+        const="arg_was_not_given",
+        help="Name of the author(s)",
+    )
+    args = parser.parse_args()
+
+    if args.title == "arg_was_not_given":
+        print("Please provide the title information")
+    elif args.title is None:
+        print("Please provide provide the title using -t argument")
+        sys.exit()
+
+    if args.author == "arg_was_not_given":
+        print("Please provide the author information")
+    elif args.author is None:
+        print("Please provide the author using -a argument")
+        sys.exit()
+
+    return args
+
+
+def get_contents(file_name):
+    blocks = list()
+    with open(file_name, "r") as f:
+        # (highlights, notes, time)
+        block = list()
+        for line in f:
+            if line == "\n":
+                continue
+            info = line.rstrip("\n")
+            # info = line
+
+            if is_date(info, fuzzy=False):
+                if len(block) == 1:
+                    block.append("")
+
+                dt = parse(info.rstrip("."))
+                block.append(dt.strftime("%Y-%m-%d 17:00:00"))
+                blocks.append(block)
+                block = list()
+                continue
+            block.append(info)
+    return blocks
+
+
+if __name__ == "__main__":
+    args = get_args()
+    file_name = args.file_name
+    blocks = get_contents(file_name)
+    df = pd.DataFrame(blocks, columns=["Highlight", "Note", "Date"])
+    df["Title"] = args.title
+    df["Author"] = args.author
+    df = df[["Highlight", "Title", "Author", "Note","Date"]]
+    df.to_csv("highlights.csv", index=False)
